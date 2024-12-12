885159446184dfa9ebbeca53e1b6e0888180b569
Mohammed Choubi <choubimohammed99@gmail.com>
2024-12-12 21:47:09 +0000

    complete MVC

---
+++ back-end/controllers/contTodo.js
@@ -0,0 +1,42 @@
+const asynchandler=require('express-async-handler')
+const Todos=require('../models/Todo')
+
+
+exports.getAlltodos=asynchandler(async (req,res)=>{
+    const todo=await Todos.find()
+    if(todo.length > 0){
+        res.status(200).json(todo)
+    }else{
+        res.status(400).json({message : "Todo Is Vide !!!!!!!!!!!!!"})
+    }
+})
+exports.addTodo=asynchandler(async (req,res)=>{
+    const newtodos=new Todos({
+        id:req.body.id,
+        title:req.body.title,
+        iscomplete:req.body.iscomplete,
+    })
+    const result= await newtodos.save()
+    res.status(201).json({message:"ajouter avec seccefuly",result})
+})
+exports.updatebyId=asynchandler(async (req,res)=>{
+    const todo_id=req.params.id
+    const todo=await Todos.findOneAndUpdate({id:todo_id},{
+        title:req.body.title,
+        iscomplete:req.body.iscomplete,
+    },{new:true})
+if(todo){
+    res.status(200).json(todo)
+}else{
+    res.status(401).json({message:"todo not found !"})
+}
+})
+exports.deletebyId=asynchandler(async (req,res)=>{
+    const todo_id=req.params.id
+    const todo=await Todos.findOneAndDelete({id:todo_id})
+    if(todo){
+        res.status(200).json({message:"todo has been deleted"})
+    }else{
+        res.status(401).json({message:"todo not found !"})
+    }
+    })
--- back-end/router/todoRouter.js
+++ back-end/router/todoRouter.js
@@ -1,45 +1,9 @@
 const express=require('express')
 const router=express.Router()
-const asynchandler=require('express-async-handler')
-const Todos=require('../models/Todo')
-
-router.get("/",asynchandler(async (req,res)=>{
-    const todo=await Todos.find()
-    if(todo.length > 0){
-        res.status(200).json(todo)
-    }else{
-        res.status(400).json({message : "Todo Is Vide !!!!!!!!!!!!!"})
-    }
-}))
-router.post("/add",asynchandler(async (req,res)=>{
-    const newtodos=new Todos({
-        id:req.body.id,
-        title:req.body.title,
-        iscomplete:req.body.iscomplete,
-    })
-    const result= await newtodos.save()
-    res.status(201).json({message:"ajouter avec seccefuly",result})
-}))
-router.put("/:id",asynchandler(async (req,res)=>{
-    const todo_id=req.params.id
-    const todo=await Todos.findOneAndUpdate({id:todo_id},{
-        title:req.body.title,
-        iscomplete:req.body.iscomplete,
-    },{new:true})
-if(todo){
-    res.status(200).json(todo)
-}else{
-    res.status(401).json({message:"todo not found !"})
-}
-}))
-router.delete("/:id",asynchandler(async (req,res)=>{
-    const todo_id=req.params.id
-    const todo=await Todos.findOneAndDelete({id:todo_id})
-    if(todo){
-        res.status(200).json({message:"todo has been deleted"})
-    }else{
-        res.status(401).json({message:"todo not found !"})
-    }
-    }))
+const todoController=require("../controllers/contTodo")
+router.get("/",todoController.getAlltodos)
+router.post("/add",todoController.addTodo)
+router.put("/:id",todoController.updatebyId)
+router.delete("/:id",todoController.deletebyId)
 
 module.exports=router;
